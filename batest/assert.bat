8a8cc2e2941fd66786ec61ba5352be0490eb266e
rbaltrusch <41642326+rbaltrusch@users.noreply.github.com>
2021-02-16 22:41:49 +0100

    Extend assert functionality
    Add support for IF EXIST FILENAME
    Add support for leading NOT (e.g. assert NOT 1 EQU 0 msg)

--- batest/assert.bat
+++ batest/assert.bat
@@ -1,8 +1,33 @@
 @echo off
-if "%~1" %~2 "%~3" (
+
+if /i "%~1" == "NOT" (
+	set not=NOT
+	shift
+) else (
+	set not=
+)
+
+set operator=%~2
+set message=%~4
+
+if "%~3" == "" (
+	set third=
+) else (
+	set third="%~3"
+)
+
+if /i "%~1" == "EXIST" (
+	set first=EXIST
+	set message=%~3
+	set third=
+) else (
+	set first="%~1"
+)
+
+if %not% %first% %~2 %third% (
 	set errorlevel=0
 )else (
 	set errorlevel=1
-	if "%~4" NEQ = "" echo %~4
+	if "%message%" NEQ = "" echo %message%
 )
 exit /b %errorlevel%
