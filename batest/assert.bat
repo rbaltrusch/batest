3f9a510c8c5243c9951f7c8ce2427816f7f450fe
rbaltrusch <41642326+rbaltrusch@users.noreply.github.com>
2021-02-19 15:58:19 +0100

    Add assert help

--- batest/assert.bat
+++ batest/assert.bat
@@ -1,5 +1,36 @@
 @echo off
 
+if /i "%~1" == "help" (
+	echo The assert statement that assert.bat provides can be used to assert that certain conditions are true.
+	echo.
+	echo General usage: CALL assert OPERAND1 OPERATOR OPERAND1 MESSAGE
+	echo.
+	echo If the assertion passes, ERRORLEVEL is set to 0. If it fails, it is set to 1 and the error message provided, if any, is outputted to console.
+	echo.
+	echo The assert statement supports a lot of the functionality that the IF statement supports. In particular, it supports the following operators:
+	echo.
+	echo     ==
+	echo     EQU
+	echo     NEQ
+	echo     GEQ
+	echo     LSS
+	echo     GTR
+	echo     NOT
+	echo     EXIST
+	echo.
+	echo For more help regarding the operators, see HELP IF.
+	echo.
+	pause
+	echo.
+	echo Examples:
+	echo.
+	echo     call assert 1 EQU 1 "This is the error message"
+	echo     call assert EXIST file.txt
+	echo     call assert NOT 1 GEQ 2
+	echo.
+	exit /b 0
+)
+
 if /i "%~1" == "NOT" (
 	set not=%~1
 	shift
