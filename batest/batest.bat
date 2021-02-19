78baaacde572bd00ecb9d654b9b8bfb68c3bba1a
rbaltrusch <41642326+rbaltrusch@users.noreply.github.com>
2021-02-19 14:18:28 +0100

    Improve test report
    Add night mode
    Add results summary with number of passes and fails
    Add test paths for each test script executed

--- batest/batest.bat
+++ batest/batest.bat
@@ -7,6 +7,9 @@ goto :main
 setlocal enabledelayedexpansion
 	set tempfile=__testfiles__.txt
 	set outputfile=__testoutput__.txt
+	set "new_path=%~1"
+	set /a numberfailed=0
+	set /a numberpassed=0
 	cd "%~1"
 	if exist tests (
 		cd tests
@@ -25,8 +28,10 @@ setlocal enabledelayedexpansion
 				call tests/%%f >%outputfile%
 				if "!errorlevel!" == "0" (
 					set status=passed
+					set /a numberpassed+=1
 				)else (
 					set status=failed
+					set /a numberfailed+=1
 				)
 				echo ^<tr class=testresult^> ^<td^>%%f^</td^> ^<td class=!status!^>!status!^</td^> >>"%~2/%~3"
 				echo ^<td class=output^> >>"%~2/%~3"
@@ -35,6 +40,7 @@ setlocal enabledelayedexpansion
 				)
 				del %outputfile%
 				echo ^</td^> >>"%~2/%~3"
+				echo ^<td^>%new_path%^</td^> >>"%~2/%~3"
 				echo ^</tr^> >>"%~2/%~3"
 			)
 		)
@@ -43,12 +49,24 @@ setlocal enabledelayedexpansion
 		if "%~4" NEQ "list" (if exist tests/teardown.bat call tests/teardown.bat)
 	)
 	cd "%~2"
-endlocal
+endlocal & set /a numberpassed=%numberpassed% & set /a numberfailed=%numberfailed%
 exit /b
 
 :main
 set "path=%path%;%~dp0"
 set testreport=test_report.html
+set css=styles.css
+set batestpath=%~dp0
+
+if "%~1" == "-n" (
+	set css=night.css
+	shift
+)
+
+if "%~1" == "--night" (
+	set css=night.css
+	shift
+)
 
 if exist "%~1" (
 	set "test_path=%~1"
@@ -59,9 +77,10 @@ cd %test_path%
 set "test_path=%cd%"
 
 echo ^<!DOCTYPE html^> >"%test_path%/%testreport%"
-echo ^<head^> ^<meta charset="utf-8"^> ^<link rel="stylesheet" href="%original_path%/css/styles.css"^> ^</head^> >>"%test_path%/%testreport%"
+echo ^<head^> ^<meta charset="utf-8"^> ^<link rel="stylesheet" href="%batestpath%/css/%css%"^> ^</head^> >>"%test_path%/%testreport%"
 echo ^<h2^>Batest report for %test_path%^</h2^> >>"%test_path%/%testreport%"
-echo ^<table^> ^<tr^> ^<th^>Testfile^</th^> ^<th^>Status^</th^> ^<th^>Output^</th^> ^</tr^> >>"%test_path%/%testreport%"
+echo ^<p^>^<i^>%date% %time% %computername%:%username%^</i^>^</p^> >>"%test_path%/%testreport%temp"
+echo ^<table^> ^<tr^> ^<th^>Testfile^</th^> ^<th^>Status^</th^> ^<th^>Output^</th^> ^<th^>Path^</th^> ^</tr^> >>"%test_path%/%testreport%temp"
 
 ::help input argument
 if "%~1" == "help" (
@@ -88,15 +107,32 @@ if "%~1" == "path" (
 	exit /b 0
 )
 
-::testing scripts
-call :run_tests "." "%test_path%" "%testreport%" "%~1"
+set /a npass=0
+set /a nfail=0
 
+::testing scripts
+call :run_tests "." "%test_path%" "%testreport%temp" "%~1"
+
+set /a npass+=%numberpassed%
+set /a nfail+=%numberfailed%
+
+setlocal enabledelayedexpansion
 for /f "delims=," %%c in ('dir /b /s "%test_path%"') do (
 	if exist %%~sc\nul (
-		call :run_tests "%%c" "%test_path%" "%testreport%" "%~1"
+		call :run_tests "%%c" "%test_path%" "%testreport%temp" "%~1"
+		set /a npass+=!numberpassed!
+		set /a nfail+=!numberfailed!
 	)
 )
 
-echo ^</table^> >>"%test_path%/%testreport%"
+echo ^<div class=results^>^<p^>^<span class=passedtext^>Passed: %npass%^</span^> ^<span class=failedtext^>Failed: %nfail% ^</span^>^</p^>^</div^> >>"%test_path%/%testreport%"
 
-cd %original_path%
+if exist "%test_path%/%testreport%temp" (
+	for /f "tokens=* delims=; usebackq" %%c in ("%test_path%/%testreport%temp") do (
+		echo %%c>>"%test_path%/%testreport%"
+	)
+	del %testreport%temp
+)
+
+echo ^</table^> >>"%test_path%/%testreport%"
+exit /b 0
