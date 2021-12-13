7ac56262b341e6d552fe73a383672506fd227a78
Richard Baltrusch <richard@baltrusch.net>
2021-12-13 23:14:53 +0100

    Add comments

--- tests/run_tests.bat
+++ tests/run_tests.bat
@@ -1,8 +1,12 @@
 @echo off
+::runs batest.bat in the batest folder. Used for a Github Actions Workflow.
+::Note that batest does not and cannot return errorlevel if a test failed (windows bug)
+::Instead, this is solved by echoing out the number of failed tests and redirecting this.
+
 set ERRORLEVEL=0
 set tempfile=__temp__
 call "../batest/batest.bat" ../batest > %tempfile%
 set /p ERRORLEVEL= < %tempfile%
 del %tempfile%
-echo %ERRORLEVEL%
+echo %ERRORLEVEL% error(s) occured.
 exit /b %ERRORLEVEL%
